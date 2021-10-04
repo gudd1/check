abe5e01bb27653abe603f0c0b315e5f0c9a92175
guddi <soumyashree.guddi@gmail.com>
2021-10-04 07:20:11 +0530

    2st commit

---
+++ testapp/scrapages.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+
+def scrap():
+    listofurls=[]
+    #for loop for urls
+    URL = "https://carsshowroom.herokuapp.com/"
+    r = requests.get(URL)
+
+    soup = BeautifulSoup(r.content, 'html5lib')
+
+    quotes = []  # a list to store quotes
+    #if URL==https://carsshowroom.herokuapp.com/
+    table = soup.find('div', attrs={'class': 'main-content'})
+    print(table)
+    for row in table.findAll('img',
+                         attrs={'class': 'car_image'}):
+        print("TTTTTt",row)
+        quote = {}
+        quote['img'] = row['src']
+        '''quote['theme'] = row.text
+        print("I'm here",row.text)'''
+        '''quote['url'] = row.a['href']
+        quote['img'] = row.img['src']
+        quote['lines'] = row.img['alt'].split(" #")[0]
+        quote['author'] = row.img['alt'].split(" #")[1]'''
+        quotes.append(quote)
+
+    filename = 'inspirational_quotes.csv'
+    #if statement for file name
+    with open(filename, 'w', newline='') as f:
+         w = csv.DictWriter(f, ['img'])
+         w.writeheader()
+         for quote in quotes:
+             w.writerow(quote)
